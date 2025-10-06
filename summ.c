94ef4f5b78c579bb0829a9fbbbd39a788e80a2af
Tanmay18forever <babbartanmay164@gmail.com>
2025-10-06 13:12:46 +0530

    Add files via upload

---
+++ summ.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+ {
+    int n; 
+    int sum = 0; 
+    scanf("%d", &n); 
+    if (n <= 0) 
+    {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+    for (int i = 1; i <= n; i++)
+     {
+        sum += i; 
+     }
+    printf("The sum of the first %d natural numbers is: %d\n", n, sum);
+
+    return 0; 
+}
